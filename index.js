3d0fd411952a98a26bf50526dca85f34c8ae8905
Anuj Khanal <anuz.khanal@gmail.com>
2021-02-06 02:16:13 +0200

    added members

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const path = require('path');
+const members = require('./Members');
 
 const app = express();
+
+//Get All Members
+app.get('/api/members ', (req, res) => res.json(members));
+
+//Static Server
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
